451515bba47bc03800fb3d32b2d1c1bf8ff88ec8
fidilius <fidilius.siregar@gmail.com>
2023-06-04 12:44:40 +0700

    fix: navigate logo navbar & add transition carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -21,6 +21,8 @@ const data = [
 function Carousel() {
   const [currentSlide, setCurrentSlide] = useState(0)
 
+  const lastIndex = data.length - 1
+
   const nextSlide = () => {
     if (currentSlide === data.length - 1) {
       setCurrentSlide(0)
@@ -38,8 +40,60 @@ function Carousel() {
 
   return (
     <div className="h-60 md:h-80 border-y-4 border-purple-300">
-      <div className="relative h-full">
-        <div
+      <div className="relative h-full overflow-x-hidden">
+        {data.map((d, i) => {
+          const { img, caption, paragraph } = d
+          let position = "translate-x-full"
+
+          if (i === currentSlide) {
+            position = "opacity-100 bg-bottom"
+          }
+
+          if (
+            i === currentSlide - 1 ||
+            (currentSlide === 0 && i === lastIndex)
+          ) {
+            position = "-translate-x-full"
+          }
+
+          return (
+            <div
+              key={i}
+              className={
+                "absolute top-0 left-0 w-full h-full bg-no-repeat bg-cover  bg-fixed opacity-0 duration-1000 " +
+                position
+              }
+              style={{ backgroundImage: `url(${img})` }}
+            >
+              <div className="absolute hidden sm:block left-1/2 -translate-x-1/2 bottom-4 bg-[#00000044] rounded-md py-4 px-8">
+                <h5 className="text-purple-200 text-lg lg:text-2xl font-semibold lg:font-bold">
+                  {caption}
+                </h5>
+                <p className="text-purple-200 text-sm lg:text-lg italic">
+                  {paragraph}
+                </p>
+              </div>
+            </div>
+          )
+        })}
+        <div className="absolute top-1/2 -translate-y-1/2 w-full z-20">
+          <div className="relative h-10">
+            <button
+              className="aspect-square h-full rounded-full bg-white hover:bg-purple-200 absolute left-2 text-2xl font-bold border border-purple-300"
+              onClick={prevSlide}
+            >
+              <p className="aspect-square">&#60;</p>
+            </button>
+            <button
+              className="aspect-square h-full rounded-full bg-white hover:bg-purple-200 absolute right-2 text-2xl font-bold border border-purple-300"
+              onClick={nextSlide}
+            >
+              <p className="aspect-square">&#62;</p>
+            </button>
+          </div>
+        </div>
+
+        {/* <div
           className="w-full h-full bg-no-repeat bg-cover bg-center bg-fixed"
           style={{ backgroundImage: `url(${data[currentSlide].img})` }}
         />
@@ -66,7 +120,7 @@ function Carousel() {
               <p className="aspect-square">&#62;</p>
             </button>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,11 +13,18 @@ function Navbar() {
   return (
     <header className="bg-slate-950 sticky top-0 border-b-2 border-b-purple-500 z-50">
       <div className="container mx-auto py-2 md:py-4 md:px-[2%] flex justify-center md:justify-between">
-        <h1 className="text-xl font-semibold text-white hidden md:block">
+        <h1
+          className="text-xl font-semibold text-white hidden md:block cursor-pointer"
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        >
           Photo <span className="text-purple-300 font-bold">Collection</span>
         </h1>
         <nav className="flex justify-between gap-8 md:gap-6">
-          <NavLink to="/" className={({ isActive }) => handleActive(isActive)}>
+          <NavLink
+            to="/"
+            className={({ isActive }) => handleActive(isActive)}
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          >
             Home
           </NavLink>
           {pathname === "/" && (
@@ -39,6 +46,7 @@ function Navbar() {
           <NavLink
             to="/gallery"
             className={({ isActive }) => handleActive(isActive)}
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
           >
             Gallery
           </NavLink>
